b2a20bcb06d03c781f611c9ab18a57e6bb6d4fe0
sanghoonsim <ssh900208@gmail.com>
2021-10-05 16:00:04 -0700

    commit-first-posting

---
+++ _pages/about.md
@@ -0,0 +1,5 @@
+---
+
+permalink: /about/ title: "About" toc: true toc_sticky: true toc_label: "MYSELF"
+
+---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[Jekyll] How to post",
+        "excerpt":"test  ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/test/",
+        "teaser": null
+      }]
